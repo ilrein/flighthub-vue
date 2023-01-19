41a636192bdf7c590ee0ec9842bb87a7309d808d
Ilia Reingold <ilia.reingold@gmail.com>
2023-01-19 12:12:40 -0500

    init

---
+++ src/stories/ISidebar.stories.js
@@ -0,0 +1,22 @@
+import ISidebar from '../components/ISidebar.vue';
+
+export default {
+  title: 'ISidebar',
+  component: ISidebar,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/vue/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+const Template = () => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { ISidebar },
+
+  // Here we define the `template`
+  template: '<i-sidebar />',
+});
+
+// More on interaction testing: https://storybook.js.org/docs/vue/writing-tests/interaction-testing
+export const Default = Template.bind({});
+Default.args = {};
