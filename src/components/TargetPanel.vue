392c4136ba76b3b0325efacbadb0b79899c9926c
Ilia Reingold <ilia.reingold@gmail.com>
2023-01-27 11:21:41 -0500

    multiselect added

--- src/components/TargetPanel.vue
+++ src/components/TargetPanel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4">
+  <div>
     <div class="font-bold text-4xl mb-4">
       Target
     </div>
@@ -9,12 +9,39 @@
           Dates
         </div>
         <div>
-          <Datepicker
-            v-model="date"
-            :enable-time-picker="false"
-            range
-            space-confirm
-          />
+          <Datepicker v-model="date" :enable-time-picker="false" range space-confirm />
+        </div>
+      </div>
+
+      <div class="mt-4">
+        <div class="text-base font-bold">
+          Insertion
+        </div>
+        <div>
+          <select class="w-full border rounded p-2">
+            <option v-for="option in props.insertionOptions" :key="option.value">
+              {{ option.label }}
+            </option>
+          </select>
+        </div>
+      </div>
+
+      <div class="mt-4">
+        <div class="text-base font-bold">
+          Shows
+        </div>
+        <div>
+          <!-- <select class="w-full border rounded p-2">
+            <option
+              v-for="option in props.shows"
+              :key="option.value"
+            >
+              {{ option.label }}
+            </option>
+          </select> -->
+          <Multiselect v-model="shows" :multiple="true" :options="props.shows">
+            <template v-slot:selection="{ values, isOpen }"><span class="multiselect__single" v-if="values.length" v-show="!isOpen">{{ values.length }} options selected</span></template>
+          </Multiselect>
         </div>
       </div>
     </div>
@@ -27,10 +54,75 @@ import { ref } from 'vue'
 // import { ChevronDownIcon } from '@heroicons/vue/20/solid'
 import Datepicker from '@vuepic/vue-datepicker';
 import { addDays } from 'date-fns'
+import Multiselect from 'vue-multiselect'
+
+import 'vue-multiselect/dist/vue-multiselect.css'
 import '@vuepic/vue-datepicker/dist/main.css'
 
+const props = defineProps({
+  startDate: {
+    type: Date,
+    default: new Date(),
+  },
+  endDate: {
+    type: Date,
+    default: addDays(new Date(), 7),
+  },
+  insertionOptions: {
+    type: Array,
+    default: () => [{
+      label: 'Pre + Mid',
+      value: 'pre-mid',
+    }],
+  },
+  shows: {
+    type: Array,
+    default: () => [
+      {
+        label: 'Show 1',
+        value: 'show-1',
+      },
+      {
+        label: 'Show 2',
+        value: 'show-2',
+      },
+      {
+        label: 'Show 3',
+        value: 'show-3',
+      },
+    ],
+  },
+  geoRestrictions: {
+    type: Array,
+    default: () => [
+      {
+        label: 'Canada',
+        value: 'canada',
+      }
+    ]
+  },
+  maxImpressions: {
+    type: Number,
+    default: null,
+  },
+  maxImpressionsTimeLimit: {
+    type: Number,
+    default: null,
+  },
+  positionLocked: {
+    type: String,
+    default: null,
+  }
+})
+
 const date = ref([new Date(), addDays(new Date(), 7)])
+
+const shows = ref()
+
+console.log(props.maxImpressions, props);
+
 </script>
 
 <style lang="postcss">
+
 </style>
--- src/features/PrecogSalesDisplay.ce.vue
+++ src/features/PrecogSalesDisplay.ce.vue
@@ -1,15 +1,87 @@
 <template>
   <div class="font-poppins">
     <FlowTabs active-tab="run-of-show" />
-    <div class="grid">
-      <TargetPanel />
+    <div class="grid grid-cols-6">
+      <div class="col-span-2 p-4">
+        <TargetPanel
+          :startDate="props.startDate"
+          :endDate="props.endDate"
+          :insertionOptions="props.insertionOptions"
+          :shows="props.shows"
+          :geoRestrictions="props.geoRestrictions"
+          :maxImpressions="props.maxImpressions"
+          :maxImpressionsTimeLimit="props.maxImpressionsTimeLimit"
+          :positionLocked="props.positionLocked"
+        />
+      </div>
+      <div class="col-span-4 p-4">
+        body
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { addDays } from 'date-fns'
+
 import FlowTabs from '../components/FlowTabs.vue'
 import TargetPanel from '../components/TargetPanel.vue';
+
+const props = defineProps({
+  startDate: {
+    type: Date,
+    default: new Date(),
+  },
+  endDate: {
+    type: Date,
+    default: addDays(new Date(), 7),
+  },
+  insertionOptions: {
+    type: Array,
+    default: () => [{
+      label: 'Pre + Mid',
+      value: 'pre-mid',
+    }],
+  },
+  shows: {
+    type: Array,
+    default: () => [
+      {
+        label: 'Show 1',
+        value: 'show-1',
+      },
+      {
+        label: 'Show 2',
+        value: 'show-2',
+      },
+      {
+        label: 'Show 3',
+        value: 'show-3',
+      },
+    ],
+  },
+  geoRestrictions: {
+    type: Array,
+    default: () => [
+      {
+        label: 'Canada',
+        value: 'canada',
+      }
+    ]
+  },
+  maxImpressions: {
+    type: Number,
+    default: null,
+  },
+  maxImpressionsTimeLimit: {
+    type: Number,
+    default: null,
+  },
+  positionLocked: {
+    type: String,
+    default: null,
+  }
+})
 </script>
 
 <style lang='postcss'>
